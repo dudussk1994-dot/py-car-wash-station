52295413557c695d0bd65efa510d09c611a547ff
Damian <dudussk@go2.pl>
2026-02-04 15:45:44 +0100

    Solution4

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int, clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -23,8 +25,8 @@ class CarWashStation:
     def calculate_washing_price(self, car: Car) -> float:
         price = 0
         if car.clean_mark < self.clean_power:
-            price = ((car.comfort_class * (
-            self.clean_power - car.clean_mark) * self.average_rating) / self.distance_from_city_center)
+            price = ((car.comfort_class * (self.clean_power - car.clean_mark)
+                      * self.average_rating) / self.distance_from_city_center)
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
@@ -37,5 +39,3 @@ class CarWashStation:
             new_sum = prev_sum + new_rate
             self.count_of_ratings += 1
             self.average_rating = round(new_sum / self.count_of_ratings, 1)
-
-
