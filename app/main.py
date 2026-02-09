bf1f37ee87e5b9ae758f9cc00f0d6a32462d7d9a
Damian <dudussk@go2.pl>
2026-02-09 13:22:39 +0100

    Solution10

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 class Car:
+    # write your code here
+    pass
     def __init__(self, comfort_class: int, clean_mark: int,
                  brand: str) -> None:
         self.comfort_class = comfort_class
@@ -7,6 +9,9 @@ class Car:
 
 
 class CarWashStation:
+    # write your code here
+    pass
+
     def __init__(self, distance_from_city_center: float, clean_power: int,
                  average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
